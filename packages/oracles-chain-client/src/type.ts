0274c9bd524e3d094945d62ddfb54269011f9cef
yousefhany77 <yousefhany2014@gmail.com>
2025-08-04 22:24:18 +0300

    refactor: remove unused Veramo dependencies from oracles-chain-client
    - Deleted multiple Veramo-related dependencies from package.json in the oracles-chain-client package to streamline the codebase.
    - Cleaned up type definitions in type.ts by removing unused imports and interfaces related to Veramo, enhancing clarity and maintainability.

--- packages/oracles-chain-client/src/type.ts
+++ packages/oracles-chain-client/src/type.ts
@@ -1,40 +1,14 @@
-import { type DeliverTxResponse } from '@cosmjs/stargate';
-import {
-  type ICreateVerifiableCredentialArgs,
-  type VerifiableCredential,
-} from '@veramo/core';
-
 export type Networks = 'devnet' | 'testnet' | 'mainnet';
-export type * from '@veramo/core';
-
-export interface IClaim {
-  claimId: string;
-  // Used for schema evaluation
-  credentials?: VerifiableCredential;
-}
 
 export interface ISubmitClaimPayload {
   type: string; // schema type to validate against
   collectionId: string;
-  claims: IClaim[];
-}
-
-export interface ICreateAndSubmitClaimPayload {
-  type?: string; // schema type to validate against
-  collectionId: string;
-  storage: 'cellnode' | 'ipfs';
-  // must provide either credentials or generate
-  credential: ICreateVerifiableCredentialArgs;
 }
 
 export interface ISubmitClaim {
   type: string; // schema type to validate against
   collectionId: string;
-  claims: IClaim[];
 }
-export type CreateAndSubmitResponse = {
-  claims: { claimId: string; credentials: VerifiableCredential }[];
-} & DeliverTxResponse;
 
 export enum AgentRoles {
   evaluators = 'EA',
